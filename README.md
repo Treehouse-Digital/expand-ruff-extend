04cfef8ce7cd6e2d2fa0164b23e81f1688a078df
Joseph Yu <joseph.yu@treehousevfx.studio>
2026-01-06 15:36:56 +0000

    0.2.0: skip on CI by default

--- README.md
+++ README.md
@@ -6,10 +6,18 @@ usage: expand-ruff-extend [-h] [-i I] [-o O] [-v]
 Expand ruff config's extend attribute and write it out.
 
 options:
-  -h, --help  show this help message and exit
-  -i I        Input config file name (at git repo root)
-  -o O        Output config file name (at git repo root)
-  -v          Verbosity (of LOGGER). WARNING by default, -v: INFO, -vv: DEBUG
+  -h, --help    show this help message and exit
+  -v            Verbosity (of LOGGER). WARNING by default, -v: INFO, -vv: DEBUG
+  -i I          Input config file name (at git repo root)
+  -o O          Output config file name (at git repo root)
+  --no-ci-skip  Force to still run even on CI
+
+By default, it read's current git repo's root pyproject.toml and writes out
+(without comments or previous formatting) to .ruff.toml
+
+This is useful to flatten ruff configs e.g. when "extend" points to a local config
+which isn't available on CI/CD. Therefore nothing is done if $CI variable is set to
+a non-empty and true-ish value unless --no-ci-skip is explicitly passed in.
 ```
 
 By default, it read's current git repo's root pyproject.toml and writes out
@@ -24,7 +32,7 @@ Also available as a [pre-commit](https://pre-commit.com/#usage) hook:
 # .pre-commit-config.yaml
 repos:
 - repo: https://github.com/Treehouse-Digital/expand-ruff-extend
-  rev: 0.1.0
+  rev: 0.2.0
   hooks: 
     - id: expand-ruff-extend
 ```
--- expand_ruff_extend/_cli.py
+++ expand_ruff_extend/_cli.py
@@ -5,11 +5,13 @@ By default, it read's current git repo's root pyproject.toml and writes out
 (without comments or previous formatting) to .ruff.toml
 
 This is useful to flatten ruff configs e.g. when "extend" points to a local config
-which isn't available on CI/CD.
+which isn't available on CI/CD. Therefore nothing is done if $CI variable is set to
+a non-empty and true-ish value unless --no-ci-skip is explicitly passed in.
 """
 
 import argparse
 import logging
+import os
 from collections.abc import Sequence
 from pathlib import Path
 
@@ -17,6 +19,12 @@ from .constants import DEFAULT_IN, DEFAULT_OUT
 from .git import get_repo_root
 from .logger import LOGGER
 from .ruff_configs import expanded_contents
+
+
+def on_ci() -> bool:
+    """Whether we're on CI."""
+    lower_true_values = ("true", "t", "1", "yes", "y", "on")
+    return (value := os.getenv("CI")) and value.lower() in lower_true_values
 
 
 def main(argv: Sequence[str] | None = None) -> None:
@@ -34,16 +42,22 @@ def main(argv: Sequence[str] | None = None) -> None:
     config_suffix = " config file name (at git repo root)"
     parser.add_argument("-i", help=f"Input {config_suffix}", default=DEFAULT_IN)
     parser.add_argument("-o", help=f"Output {config_suffix}", default=DEFAULT_OUT)
+    parser.add_argument(
+        "--no-ci-skip", action="store_true", help="Force to still run even on CI"
+    )
 
     args = parser.parse_args(argv)
     LOGGER.setLevel([logging.WARNING, logging.INFO, logging.DEBUG][args.v])
-    repo_root = get_repo_root()
-    if out_text := expanded_contents(repo_root / args.i):
-        out_toml = repo_root / args.o
-        out_toml.write_text(out_text)
-        LOGGER.info(f"Written expanded to: {out_toml}")
+    if on_ci() and not args.no_ci_skip:
+        LOGGER.info("Skipping as we're on CI and --no-ci-skip not passed in")
     else:
-        LOGGER.warning("No expanded ruff configs to write")
+        repo_root = get_repo_root()
+        if out_text := expanded_contents(repo_root / args.i):
+            out_toml = repo_root / args.o
+            out_toml.write_text(out_text)
+            LOGGER.info(f"Written expanded to: {out_toml}")
+        else:
+            LOGGER.warning("No expanded ruff configs to write")
 
 
 if __name__ == "__main__":
